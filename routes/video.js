6919124dd708f8cb99e4fecebcf126bdbc2679d5
Argo Roots <argo@roots.ee>
2016-03-09 06:35:26 +0200

    Video list page

--- routes/video.js
+++ routes/video.js
@@ -3,6 +3,16 @@ var router   = require('express').Router()
 
 
 router.get('/', function(req, res) {
+    res.render('videolist', {
+        result: true,
+        version: APP_VERSION,
+        started: APP_STARTED
+    })
+})
+
+
+
+router.get('/:id', function(req, res) {
     res.render('video', {
         result: true,
         version: APP_VERSION,
