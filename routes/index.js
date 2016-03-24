3a41c18d5bf5e8b9a63a98378180a91d39d6ea54
Argo Roots <argo@roots.ee>
2016-03-24 07:42:23 +0200

    Supporters from Entu

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,61 @@
-var router   = require('express').Router()
+var async  = require('async')
+var router = require('express').Router()
+
+var entu   = require('../helpers/entu')
 
 
 
 router.get('/', function(req, res) {
-    res.render('index/index', {
-        pageUrl: req.protocol + '://' + req.get('host') + req.originalUrl
+    async.parallel({
+        partners1: function(callback) {
+            entu.getEntities({
+                parentEntityId: 1178,
+                definition: 'institution',
+                fullObject: false
+            }, callback)
+        },
+        partners2: function(callback) {
+            entu.getEntities({
+                parentEntityId: 1178,
+                definition: 'person',
+                fullObject: false
+            }, callback)
+        },
+        sponsors: function(callback) {
+            entu.getEntities({
+                parentEntityId: 1179,
+                definition: 'institution',
+                fullObject: false
+            }, callback)
+        },
+        thanks: function(callback) {
+            entu.getEntities({
+                parentEntityId: 1180,
+                definition: 'person',
+                fullObject: false
+            }, callback)
+        },
+        team: function(callback) {
+            entu.getEntities({
+                parentEntityId: 1863,
+                definition: 'person',
+                fullObject: false
+            }, callback)
+        },
+    },
+    function(err, results) {
+        if (err) return next(err)
+
+        results.partners = results.partners1.concat(results.partners2)
+        delete results.partners1
+        delete results.partners2
+
+        console.log(results.partners.length);
+
+        results.pageUrl = req.protocol + '://' + req.get('host') + req.originalUrl
+
+        res.render('index/index', results)
     })
-})
-
-
-
-router.get('/test', function() {
-    throw new Error('böö')
 })
 
 
