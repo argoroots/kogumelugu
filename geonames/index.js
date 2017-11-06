0607d6d5a83c9ed8fc2281e68d8622d34b91ceea
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2017-11-06 11:40:19 +0200

    4 spaces

--- geonames/index.js
+++ geonames/index.js
@@ -7,65 +7,65 @@ const YAML = require('yamljs')
 const fetch = require('./geofetch.js')
 
 const rl = readline.createInterface({
-  input: require('fs').createReadStream('places.txt'),
-  // output: process.stdout
+    input: require('fs').createReadStream('places.txt'),
+    // output: process.stdout
 })
 
 var places = []
 
 rl.on('line', function (line) {
-  places.push(line)
+    places.push(line)
 })
 
 rl.on('close', () => {
-  async.eachLimit(places, 1, (place, callback) => {
-    console.log('Processing place "' + place + '"')
+    async.eachLimit(places, 1, (place, callback) => {
+        console.log('Processing place "' + place + '"')
 
-    if( place.length === 0 ) {
-      console.log('Place with no name')
-      callback()
-    } else {
-      fetch(place, (placeData) => {
-        console.log('Place "' + place + '" processed')
-        // console.log('PlaceData', YAML.stringify(placeData))
-        let filename = path.join('data', place + '.yaml')
-        csvWrite(placeData)
-        // fs.writeFile(filename, YAML.stringify(placeData), (err) => {
-        //   if (err) throw err
-        // })
-        setTimeout(function () {
-          callback()
-        }, 20)
-      })
-    }
-  }, (err) => {
-    if( err ) {
-      console.log('A place failed to process')
-    } else {
-      console.log('All places have been processed successfully')
-    }
-  })
+        if( place.length === 0 ) {
+            console.log('Place with no name')
+            callback()
+        } else {
+            fetch(place, (placeData) => {
+                console.log('Place "' + place + '" processed')
+                // console.log('PlaceData', YAML.stringify(placeData))
+                let filename = path.join('data', place + '.yaml')
+                csvWrite(placeData)
+                // fs.writeFile(filename, YAML.stringify(placeData), (err) => {
+                //     if (err) throw err
+                // })
+                setTimeout(function () {
+                    callback()
+                }, 20)
+            })
+        }
+    }, (err) => {
+        if( err ) {
+            console.log('A place failed to process')
+        } else {
+            console.log('All places have been processed successfully')
+        }
+    })
 })
 
 
 FIELDS = [ 'query', 'lng', 'lat', 'toponymName', 'geonameId', 'name-en',
-  'countryId', 'countryName-en', 'name-et', 'countryName-et', 'name-ru',
-  'countryName-ru' ]
+    'countryId', 'countryName-en', 'name-et', 'countryName-et', 'name-ru',
+    'countryName-ru' ]
 const CSVSTREAM = fs.createWriteStream('places.csv')
 CSVSTREAM.write(FIELDS.join(', ') + '\n')
 
 const csvWrite = function csvWrite(place) {
-  CSVSTREAM.write(
-    FIELDS
-      .map(function(field) {
-        let ret_val = place[field] ? place[field] : ''
-        try {
-          return '"' + ret_val.replace(/"/g, '""') + '"'
-        } catch (e) {
-          console.log({ place:place, field:field, ret_val:ret_val })
-          throw e
-        }
-      })
-      .join(', ') + '\n'
-  )
+    CSVSTREAM.write(
+        FIELDS
+            .map(function(field) {
+                let ret_val = place[field] ? place[field] : ''
+                try {
+                    return '"' + ret_val.replace(/"/g, '""') + '"'
+                } catch (e) {
+                    console.log({ place:place, field:field, ret_val:ret_val })
+                    throw e
+                }
+            })
+            .join(', ') + '\n'
+    )
 }
