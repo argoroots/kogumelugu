32117f0ce819a762af96fd557e27376ff80ba912
Argo Roots <argo@roots.ee>
2017-11-16 12:39:15 +0200

    Reuse tmp files

--- build.sh
+++ build.sh
@@ -22,39 +22,36 @@ npm install -q vimeo
 
 echo
 echo --------- FETCH
-export ENTU_QUERY="_type.string.regex=tag&props=name_et,name_en,name_ru&limit=10000"
-./node_modules/entu-ssg/helpers/entu2yaml.js ${SOURCE_DIR}/tags/tags.yaml
+export TAGS_YAML=${SOURCE_DIR}/tags/tags.yaml
+export ENTU_QUERY="_type.string=tag&props=name_en,name_et,name_ru&limit=10000"
+node ./node_modules/entu-ssg/helpers/entu2yaml.js ${TAGS_YAML}
+
+export REGIONS_YAML=${SOURCE_DIR}/tags/regions.yaml
+export ENTU_QUERY="_type.string=region&props=lat,lng,name_en,name_et,name_ru&limit=10000"
+node ./node_modules/entu-ssg/helpers/entu2yaml.js ${REGIONS_YAML}
+
+export PERSONS_YAML=${SOURCE_DIR}/tags/persons.yaml
+export ENTU_QUERY="_type.string=person&props=forename,surname&limit=10000"
+node ./node_modules/entu-ssg/helpers/entu2yaml.js ${PERSONS_YAML}
 
 export VIDEOS_YAML=${TMP_DIR}/videos.yaml
-export ENTU_QUERY="_type.string.regex=interview|story&limit=10000"
-./node_modules/entu-ssg/helpers/entu2yaml.js ${VIDEOS_YAML}
-
-export REGIONS_YAML=${TMP_DIR}/regions.yaml
-export ENTU_QUERY="_type.string=region&props=lat,lng,name_et,name_en,name_ru&limit=10000"
-./node_modules/entu-ssg/helpers/entu2yaml.js ${REGIONS_YAML}
+export ENTU_QUERY="_type.string.regex=interview|story&props=author,category,description_en,description_et,description_ru,interview,language,path,photo,project,storyteller,subject,subtitle_en,subtitle_et,subtitle_ru,title_en,title_et,title_ru,videoUrl&limit=10000"
+node ./node_modules/entu-ssg/helpers/entu2yaml.js ${VIDEOS_YAML}
 
 export TCREGIONS_YAML=${TMP_DIR}/timecodedRegions.yaml
 export ENTU_QUERY="_type.string=timecodedregion&props=_parent,region,time&limit=10000"
-./node_modules/entu-ssg/helpers/entu2yaml.js ${TCREGIONS_YAML}
-
-export PERSONS_YAML=${TMP_DIR}/persons.yaml
-export ENTU_QUERY="_type.string=person&limit=10000"
-./node_modules/entu-ssg/helpers/entu2yaml.js ${PERSONS_YAML}
-
-export TAGS_YAML=${TMP_DIR}/persons.yaml
-export ENTU_QUERY="_type.string=tag&limit=10000"
-./node_modules/entu-ssg/helpers/entu2yaml.js ${TAGS_YAML}
+node ./node_modules/entu-ssg/helpers/entu2yaml.js ${TCREGIONS_YAML}
 
 
 echo
-echo --------- MARKERS - join videos, regions and timecodedRegionsregions
+echo --------- JOIN
 export VIDEO_DATA_YAML=${SOURCE_DIR}/video/data.yaml
 node ./join_regions.js
 
 
 echo
 echo --------- BUILD
-./node_modules/entu-ssg/build.js ./entu-ssg.yaml
+node ./node_modules/entu-ssg/build.js ./entu-ssg.yaml
 
 
 echo
@@ -62,5 +59,10 @@ echo --------- PICTURES
 export PICTURES_YAML=${SOURCE_DIR}/video/data.yaml
 export PICTURES_DIR=${BUILD_DIR}/assets/images
 node ./fetch_pictures.js
+
+
+rm -rf ${TMP_DIR}
+
+
 echo
 echo --------- DONE
